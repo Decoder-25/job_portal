6b118f0f64bc004b8336e46ca633c409e09cd5e9
Disha <bidisha2125@gmail.com>
2023-06-25 02:18:26 +0530

    Added authMiddleware for two step verification of getting the token

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { testPostConntroller } from '../controllers/testController.js';
+import userAuth from '../middlewares/authMiddleware.js';
 
 //router.object
 const router = express.Router();
 
 //test
-router.post('/test-post', testPostConntroller);
+router.post('/test-post',userAuth, testPostConntroller);
 
 
 //export
