3196ac6894c549f9da0dc223cf4d4614da6e2cbf
Kheyali15 <kheyalidhara15@gmail.com>
2023-07-07 20:52:09 +0530

    commmitting welcome.js

---
+++ frontend/src/componeent/Welcome/Welcome.js
@@ -0,0 +1,25 @@
+const Welcome = (props) => {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to Job Portal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, consequatur ad nihil aliquam placeat recusandae labore libero itaque ipsum quibusdam sunt? Veritatis dolores id, aut totam aperiam dolor sequi sunt.</p>
+      </div>
+      <div>
+        <img src="" alt="image" />
+      </div>
+    </div>
+  );
+};
+
+export const ErrorPage = (props) => {
+  return (
+    <div>
+      <div>
+        <h2>ERROR 404</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
